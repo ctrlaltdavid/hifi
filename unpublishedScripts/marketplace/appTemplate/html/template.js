7ad386769963ac8ed0a60f2c98c6845000d13e38
David Rowe <david@ctrlaltstudio.com>
2018-05-23 09:18:31 +1200

    Finish addressing SVG not being handled by CDN

--- unpublishedScripts/marketplace/appTemplate/html/template.js
+++ unpublishedScripts/marketplace/appTemplate/html/template.js
@@ -29,12 +29,8 @@ this.ui = (function () {
 
     function onLoad() {
         var ICON_DIV = "icon",
-            LOGO_IMG = "logo",
-            LOGO_IMG_ACTIVE = "../assets/app-button-a.svg",
-            LOGO_IMG_INACTIVE = "../assets/app-button-i.svg",
             TOGGLE_ACTIVE_BUTTON_ID = "toggle-active",
             iconDiv,
-            logoImg,
             toggleActiveButton;
 
         // Initial button active state is communicated via URL parameter.
@@ -43,11 +39,9 @@ this.ui = (function () {
         // Set UI elements per active state.
         iconDiv = document.getElementById(ICON_DIV);
         iconDiv.className = isActive ? "on" : "off";
-        logoImg = document.getElementById(LOGO_IMG);
-        logoImg.src = isActive ? LOGO_IMG_ACTIVE : LOGO_IMG_INACTIVE;
         toggleActiveButton = document.getElementById(TOGGLE_ACTIVE_BUTTON_ID);
         toggleActiveButton.value = isActive ? "TURN OFF TEMPLATE" : "TURN ON TEMPLATE";
-        toggleActiveButton.className = isActive ? "on" : "off";
+        toggleActiveButton.className = isActive ? "red" : "blue";
         toggleActiveButton.addEventListener("click", onToggleActiveClick, true);
     }
 
