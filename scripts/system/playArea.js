b14d7cc2d66d11f908fe32b5c00756053a3278df
David Rowe <david@ctrlaltstudio.com>
2018-07-21 15:09:44 +1200

    Fade play area to blue then away when no longer facing backward

--- scripts/system/playArea.js
+++ scripts/system/playArea.js
@@ -30,6 +30,7 @@
 
         COLORS_TELEPORT_CAN_TELEPORT = { red: 97, green: 247, blue: 255 }, // Same as in teleport.js.
         COLORS_TELEPORT_CAN_TELEPORT_WARNING = { red: 255, green: 97, blue: 97 }, // Same as in teleport.js.
+        hsvColorsTeleportCanTeleportWarning,
 
         isSensorWarning = false,
         SENSOR_ORIENTATION_WARNING_DEGREES = 150,
@@ -40,7 +41,69 @@
         DEFAULT_FOOT_OFFSET = 1.0,
         playAreaFootOffset = DEFAULT_FOOT_OFFSET,
 
-        playAreaSensorPositions = [];
+        playAreaSensorPositions = [],
+
+        fadeTimer = null,
+        fadeFactor,
+        PLAY_AREA_FADE_OK_DURATION = 1000,
+        PLAY_AREA_FADE_OUT_DURATION = 500,
+        PLAY_AREA_FADE_INTERVAL = 25,
+        PLAY_AREA_FADE_OK_FACTOR = PLAY_AREA_FADE_INTERVAL / PLAY_AREA_FADE_OK_DURATION,
+        PLAY_AREA_FADE_OUT_FACTOR = PLAY_AREA_FADE_INTERVAL / PLAY_AREA_FADE_OUT_DURATION,
+        PLAY_AREA_BOX_ALPHA = 1.0,
+        PLAY_AREA_SENSOR_ALPHA = 0.8;
+
+    function hsvToRGB(hsv) {
+        // https://en.wikipedia.org/wiki/HSL_and_HSV
+        var c, h, x, rgb, m;
+
+        c = hsv.v * hsv.s;
+        h = hsv.h * 6.0;
+        x = c * (1 - Math.abs(h % 2 - 1));
+        if (0 <= h && h <= 1) {
+            rgb = { red: c, green: x, blue: 0 };
+        } else if (1 < h && h <= 2) {
+            rgb = { red: x, green: c, blue: 0 };
+        } else if (2 < h && h <= 3) {
+            rgb = { red: 0, green: c, blue: x };
+        } else if (3 < h && h <= 4) {
+            rgb = { red: 0, green: x, blue: c };
+        } else if (4 < h && h <= 5) {
+            rgb = { red: x, green: 0, blue: c };
+        } else {
+            rgb = { red: c, green: 0, blue: x };
+        }
+        m = hsv.v - c;
+        rgb = {
+            red: Math.round((rgb.red + m) * 255),
+            green: Math.round((rgb.green + m) * 255),
+            blue: Math.round((rgb.blue + m) * 255)
+        };
+        return rgb;
+    }
+
+    function rgbToHSV(rgb) {
+        // https://en.wikipedia.org/wiki/HSL_and_HSV
+        var mMax, mMin, c, h, v, s;
+
+        mMax = Math.max(rgb.red, rgb.green, rgb.blue);
+        mMin = Math.min(rgb.red, rgb.green, rgb.blue);
+        c = mMax - mMin;
+
+        if (c === 0) {
+            h = 0;
+        } else if (mMax === rgb.red) {
+            h = ((rgb.green - rgb.blue) / c) % 6;
+        } else if (mMax === rgb.green) {
+            h = (rgb.blue - rgb.red) / c + 2;
+        } else {
+            h = (rgb.red - rgb.green) / c + 4;
+        }
+        h = h / 6;
+        v = mMax / 255;
+        s = v === 0 ? 0 : c / mMax;
+        return { h: h, s: s, v: v };
+    }
 
     function isOculusHMD() {
         // FIXME: Need API method to determine HMD type.
@@ -76,7 +139,7 @@
         playAreaOverlay = Overlays.addOverlay("image3d", {
             url: Script.resolvePath("./assets/images/play-area.png"),
             color: COLORS_TELEPORT_CAN_TELEPORT_WARNING,
-            alpha: 1.0,
+            alpha: PLAY_AREA_BOX_ALPHA,
             drawInFront: false,
             visible: false
         });
@@ -92,7 +155,7 @@
                 sensorOverlay = Overlays.addOverlay("shape", {
                     shape: "Cylinder",
                     color: COLORS_TELEPORT_CAN_TELEPORT_WARNING,
-                    alpha: 0.8,
+                    alpha: PLAY_AREA_SENSOR_ALPHA,
                     dimensions: PLAY_AREA_SENSOR_OVERLAY_DIMENSIONS,
                     parentID: playAreaOverlay,
                     localRotation: PLAY_AREA_SENSOR_OVERLAY_ROTATION,
@@ -120,21 +183,88 @@
     }
 
     function setPlayAreaVisible(visible) {
-        Overlays.editOverlay(playAreaOverlay, { visible: visible });
+        Overlays.editOverlay(playAreaOverlay, {
+            visible: visible,
+            color: COLORS_TELEPORT_CAN_TELEPORT_WARNING,
+            alpha: visible ? PLAY_AREA_BOX_ALPHA : 0
+        });
         for (var i = 0; i < playAreaSensorPositionOverlays.length; i++) {
-            Overlays.editOverlay(playAreaSensorPositionOverlays[i], { visible: visible });
+            Overlays.editOverlay(playAreaSensorPositionOverlays[i], {
+                visible: visible,
+                color: COLORS_TELEPORT_CAN_TELEPORT_WARNING,
+                alpha: visible ? PLAY_AREA_SENSOR_ALPHA : 0
+            });
         }
+    }
+
+    function fadeOut() {
+        var i, length;
+
+        fadeFactor = fadeFactor - PLAY_AREA_FADE_OUT_FACTOR;
+        if (fadeFactor > 0) {
+            Overlays.editOverlay(playAreaOverlay, { alpha: fadeFactor * PLAY_AREA_BOX_ALPHA });
+            var sensorAlpha = fadeFactor * PLAY_AREA_SENSOR_ALPHA;
+            for (i = 0, length = playAreaSensorPositionOverlays.length; i < length; i++) {
+                Overlays.editOverlay(playAreaSensorPositionOverlays[i], { alpha: sensorAlpha });
+            }
+            fadeTimer = Script.setTimeout(fadeOut, PLAY_AREA_FADE_INTERVAL);
+        } else {
+            setPlayAreaVisible(false);
+        }
+    }
+
+    function fadeToOK() {
+        var color,
+            hsv,
+            i, length;
+
+        fadeFactor = fadeFactor - PLAY_AREA_FADE_OK_FACTOR;
+        if (fadeFactor > 0) {
+            color = {
+                red: fadeFactor * COLORS_TELEPORT_CAN_TELEPORT_WARNING.red
+                    + (1 - fadeFactor) * COLORS_TELEPORT_CAN_TELEPORT.red,
+                green: fadeFactor * COLORS_TELEPORT_CAN_TELEPORT_WARNING.green
+                    + (1 - fadeFactor) * COLORS_TELEPORT_CAN_TELEPORT.green,
+                blue: fadeFactor * COLORS_TELEPORT_CAN_TELEPORT_WARNING.blue
+                    + (1 - fadeFactor) * COLORS_TELEPORT_CAN_TELEPORT.blue
+            };
+            hsv = rgbToHSV(color);
+            hsv.s = Math.abs((1 - 2 * (1 - fadeFactor)) * hsvColorsTeleportCanTeleportWarning.s);
+            color = hsvToRGB(hsv);
+            Overlays.editOverlay(playAreaOverlay, { color: color });
+            for (i = 0, length = playAreaSensorPositionOverlays.length; i < length; i++) {
+                Overlays.editOverlay(playAreaSensorPositionOverlays[i], { color: color });
+            }
+            fadeTimer = Script.setTimeout(fadeToOK, PLAY_AREA_FADE_INTERVAL);
+        } else {
+            fadeFactor = 1.0;
+            fadeOut();
+        }
+    }
+
+    function cancelFade() {
+        if (fadeTimer !== null) {
+            Script.clearTimeout(fadeTimer);
+            fadeTimer = null;
+        }
+    }
+
+    function fadePlayArea() {
+        fadeFactor = 1.0;
+        fadeToOK();
     }
 
     function displayPlayArea(display) {
         if (display) {
             // Display red immediately.
+            cancelFade();
             setPlayAreaVisible(true);
             isDisplayingPlayArea = true;
         } else {
             // Fade into blue and fade out.
-            setPlayAreaVisible(false);
+            fadePlayArea();
             isDisplayingPlayArea = false;
+            // Play area is set invisible after fading out.
         }
     }
 
@@ -220,6 +350,8 @@
             destroyOverlays();
         }
     }
+
+    hsvColorsTeleportCanTeleportWarning = rgbToHSV(COLORS_TELEPORT_CAN_TELEPORT_WARNING);
 
     setUp();
     Script.scriptEnding.connect(tearDown);
